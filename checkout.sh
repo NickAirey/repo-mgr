dce9aa41d25735982ac973bb175923511745442a
Nick Airey <nairey@deloitte.com.au>
2025-03-11 11:24:48 +1100

    port

--- checkout.sh
+++ checkout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X GET "http://localhost:8080/code/init" -H "Content-Type: application/json" -d '{"repo_url":"https://github.com/Bhavish051/Python-example.git" }'
+curl -X GET "http://localhost:8000/code/init" -H "Content-Type: application/json" -d '{"repo_url":"https://github.com/Bhavish051/Python-example.git" }'
