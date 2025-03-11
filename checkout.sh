05f9811c5e7d94d09ccb8485f9630829b3fa6fdd
Nick Airey <nairey@deloitte.com.au>
2025-03-11 11:23:24 +1100

    update repo

--- checkout.sh
+++ checkout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X GET "http://localhost:8080/code/init" -H "Content-Type: application/json" -d '{"repo_url":"https://github.com/....git" }'
+curl -X GET "http://localhost:8080/code/init" -H "Content-Type: application/json" -d '{"repo_url":"https://github.com/Bhavish051/Python-example.git" }'
